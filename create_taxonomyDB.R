0d062427bac9ce0756423e0f44082dbcd7fb4500
Philipp Sehnert <psikon86@gmail.com>
2014-05-26 16:03:12 +0200

    taxonomyDB generation skript added

--- create_taxonomyDB.R
+++ create_taxonomyDB.R
@@ -5,14 +5,14 @@ suppressPackageStartupMessages(library('optparse'))
 
 # specify the commandline interface
 option_list <- list(
-  make_option(c("-o", "--output"), dest = "output", type = "character", 
-              help = "path for taxonomy db"))
-  make_option(c("-i","--input"), dest = "input", type ="character",
-              help = "location of blastdb")
-  make_option(c("--bitscore_tolerance", ), dest = "bitscore", type = "numeric",
+  make_option("--output", dest = "output", type = "character", 
+              help = "path for taxonomy db"),
+  make_option("--input", dest = "input", type ="character",
+              help = "location of blastdb"),
+  make_option("--bitscore_tolerance", dest = "bitscore", type = "numeric",
               default = 0.90, help = "bitscore_tolerance"),
-  make_option(c("--coverage_threshold"), dest = "coverage", type = "numeric",
-              default = 0.30, help = "coverage_threshold"),
+  make_option("--coverage_threshold", dest = "coverage", type = "numeric",
+              default = 0.30, help = "coverage_threshold"))
 
 # init the commandline interface
 opt <- parse_args(OptionParser(usage = "usage: %prog [options]",
